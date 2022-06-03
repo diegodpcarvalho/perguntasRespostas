b027423500c72503a79740f2a8666d0660cdb2cb
diegodpcarvalho <diegodpcarvalho@gmail.com>
2022-06-02 22:03:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app =  express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const Pergunta = require("./database/Perguntas")
+
+connection
+    .authenticate()
+    .then(() =>{
+        console.log("Conexão feita com o banco de dados")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+    Pergunta.findAll({raw: true, oder:[
+        ['id','DESC']
+    ]}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        })
+    })
+    
+})
+
+app.get("/perguntar",(req,res) =>{
+    res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) =>{
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    })
+})
+
+app.listen(8080,()=>{console.log("app rodando")})
